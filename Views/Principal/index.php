eaa77b6b32b5f4f2c466be046caf3c3ec8d75af2
diliasanchez <dili.sanchez@unah.hn>
2022-02-23 17:34:09 -0600

    bienvenido

--- Views/Principal/index.php
+++ Views/Principal/index.php
@@ -15,7 +15,7 @@
    
 <main>
                     <div class="container-fluid">
-                        <h1 class="mt-4">Dashboard</h1>
+                        <h1 class="mt-4">Bienvenidos</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
