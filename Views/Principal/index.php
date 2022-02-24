c73f90eefa1bd29aacac7ff7e0eccda3f9cb21fa
yohelis29 <yohelisbonit@gmail.com>
2022-02-23 18:42:37 -0600

    Grafico

--- Views/Principal/index.php
+++ Views/Principal/index.php
@@ -62,7 +62,8 @@
                                 <div class="card mb-4">
                                     <div class="card-header">
                                         <i class="fas fa-chart-area mr-1"></i>
-                                        Area Chart Example...
+                                        Ejemplo de gráfico de barras
+                                    </div>
                                     </div>
                                     <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
                                 </div>
