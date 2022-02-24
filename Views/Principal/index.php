3fbdaeda802cd41a8e7d3d99e51c34ba497afcb1
diliasanchez <dili.sanchez@unah.hn>
2022-02-23 18:33:32 -0600

    Tablero

--- Views/Principal/index.php
+++ Views/Principal/index.php
@@ -17,7 +17,7 @@
                     <div class="container-fluid">
                         <h1 class="mt-4">Bienvenidos a la ferreteria Allison</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Dashboard</li>
+                            <li class="breadcrumb-item active">Tablero</li>
                         </ol>
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
