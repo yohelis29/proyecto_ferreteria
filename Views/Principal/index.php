83e0a096ff7485ac2d4afc40d9c324952b3789c2
diliasanchez <dili.sanchez@unah.hn>
2022-02-23 18:14:40 -0600

    cambio

--- Views/Principal/index.php
+++ Views/Principal/index.php
@@ -15,7 +15,7 @@
    
 <main>
                     <div class="container-fluid">
-                        <h1 class="mt-4">Bienvenidos</h1>
+                        <h1 class="mt-4">Bienvenidos a la ferreteria Allison</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
