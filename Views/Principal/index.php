e50eb658fdb7609989afbd0785c853bcae085d14
yohelis29 <yohelisbonit@gmail.com>
2022-02-23 18:24:09 -0600

    prueba

--- Views/Principal/index.php
+++ Views/Principal/index.php
@@ -71,7 +71,7 @@
                                 <div class="card mb-4">
                                     <div class="card-header">
                                         <i class="fas fa-chart-bar mr-1"></i>
-                                        Bar Chart Example
+                                        Ejemplo de gráfico de barras
                                     </div>
                                     <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
                                 </div>
