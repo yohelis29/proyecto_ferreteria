77e764a22d46d32cbb6b06e7abeafb50fea11d29
YeseniaEsp29 <80561970+YeseniaEsp29@users.noreply.github.com>
2022-03-16 21:17:07 -0600

    Update ComprasModel.php

--- Models/ComprasModel.php
+++ Models/ComprasModel.php
@@ -60,6 +60,57 @@ class ComprasModel extends Query{
     }
     return $res;
    }
+   
+   public function consultarDetalle(int $id_producto, int $id_usuario)
+   {
+       $sql = "SELECT * FROM detalle WHERE id_producto = $id_producto AND id_usuario = $id_usuario ";
+       $data = $this->select($sql);
+       return $data;
+   }
+
+   public function actualizarDetalle(string $precio, int $cantidad, string $sub_total, int $id_producto, int $id_usuario){
+      
+    $sql = "UPDATE detalle SET precio = ?, cantidad = ?, sub_total = ? WHERE id_producto = ? AND id_usuario = ?";
+    $datos = array($precio,$cantidad,$sub_total,$id_producto,$id_usuario);
+    $data = $this->save($sql, $datos);
+    if ($data == 1) {
+        $res = "modificado";
+    }else{
+        $res = "error";
+    }
+    return $res;
+
+   }
+
+   public function registraCompra(string $total){
+    $sql = "INSERT INTO compras(total) VALUES (?)";
+    $datos = array($total);
+    $data = $this->save($sql, $datos);
+    if ($data == 1) {
+        $res = "ok";
+    }else{
+        $res = "error";
+    }
+    return $res;
+   }
+
+   public function id_compra(){
+       $sql = "SELECT MAX(id) AS id FROM compras";
+       $data= $this->select($sql);
+       return $data;
+   }
+
+   public function registrarDetalleCompra(int $id_compra, int $id_pro, int $cantidad, string $precio, string $sub_total){
+    $sql = "INSERT INTO detalle_compras(id_compra, id_producto, cantidad, precio, sub_total) VALUES (?,?,?,?,?)";
+    $datos = array($id_compra, $id_pro, $cantidad, $precio, $sub_total);
+    $data = $this->save($sql, $datos);
+    if ($data == 1) {
+        $res = "";
+    }else{
+        $res = "error";
+    }
+    return $res;
+  }
 
 }
 
