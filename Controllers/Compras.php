3e4433f66a60c7676a93de9b2bebd155b40182c5
yohelis29 <yohelisbonit@gmail.com>
2022-03-22 13:18:24 -0600

    Reporte Compra Realizada

--- Controllers/Compras.php
+++ Controllers/Compras.php
@@ -103,13 +103,16 @@
         public function generarPdf ($id_compra){
 
             $empresa = $this->model->getEmpresa();
+            $productos = $this->model->getProCompra($id_compra);
+            
             require('Libraries/fpdf/fpdf.php');
             $pdf = new FPDF('P', 'mm', array(80, 200));
             $pdf->AddPage();
+            $pdf->SetMargins(5,0,0);
             $pdf->SetTitle('Reporte Compra');
             $pdf->SetFont('Arial','B',14);
             $pdf->Cell(65,10, utf8_decode($empresa['nombre']), 0, 1, 'C');
-            $pdf->Image(base_url. 'Assets/img/logo.jpg',50,18,25,25);
+            $pdf->Image(base_url. 'Assets/img/logo.jpg',50,18,21,21);
             $pdf->SetFont('Arial','B',9);
             $pdf->Cell(18, 5, 'Ruc: ',0, 0, 'L');
             $pdf->SetFont('Arial','',9);
@@ -129,7 +132,29 @@
             $pdf->Cell(18, 5, utf8_decode('Folio: ') ,0, 0, 'L');
             $pdf->SetFont('Arial','',9);
             $pdf->Cell(20, 5, $id_compra,0, 1, 'L');
+            $pdf->Ln();
 
+            //Encabezado de Productos
+            $pdf->SetFillColor(0, 0, 0);
+            $pdf->SetTextColor(255, 255, 255 );
+            $pdf->Cell(10, 5, 'Cant', 0, 0, 'L', true);
+            $pdf->Cell(30, 5, utf8_decode('Descripción'), 0, 0, 'L', true);
+            $pdf->Cell(15, 5, 'Precio', 0, 0, 'L', true);
+            $pdf->Cell(15, 5, 'Sub Total', 0, 1, 'L', true);
+
+            $pdf->SetTextColor(0, 0, 0);
+            $total = 0.00;
+            foreach($productos as $row){
+                $total = $total + $row['sub_total'];
+                $pdf->Cell(10, 5, $row['cantidad'], 0, 0, 'L');
+                $pdf->Cell(30, 5, utf8_decode($row['descripcion']), 0, 0, 'L');
+                $pdf->Cell(15, 5, $row['precio'], 0, 0, 'L');
+                $pdf->Cell(15, 5, number_format($row['sub_total'], 2,'.',','), 0, 1, 'L');
+                 
+            }
+            $pdf->Ln();
+            $pdf->Cell(70, 5, 'Total a pagar', 0, 1, 'R');
+            $pdf->Cell(70, 5, number_format($total, 2, '.', ','), 0, 1, 'R');
             $pdf->Output();
         }
 
--- Models/ComprasModel.php
+++ Models/ComprasModel.php
@@ -131,6 +131,13 @@ class ComprasModel extends Query{
     return $res;
   }
 
+  public function getProCompra(int $id_compra){
+      $sql = "SELECT c.*, d.*, p.id, p.descripcion FROM compras c INNER JOIN detalle_compras d ON c.id = d.id_compra INNER JOIN productos p ON p.id = d.id_producto WHERE c.id = $id_compra";
+      $data = $this->selectAll($sql);
+      return $data;
+  
+
+    }
 }
 
 ?>
